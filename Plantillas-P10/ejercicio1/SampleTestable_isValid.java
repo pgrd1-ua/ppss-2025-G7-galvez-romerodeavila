5e5ab7d3a7ce9fffcb5a70d324ebaa7cab0e5115
Paula <pgrd1@gcloud.ua.es>
2025-05-21 17:56:58 +0200

    Subir archivos para la maq virtual

---
+++ Plantillas-P10/ejercicio1/SampleTestable_isValid.java
@@ -0,0 +1,10 @@
+package ppss.ejercicio1;
+
+public class SampleTestable_isValid extends Sample {
+    public boolean result_isValid;
+
+    @Override
+    public boolean isValid(int [] data) {
+        return result_isValid;
+    }
+}
